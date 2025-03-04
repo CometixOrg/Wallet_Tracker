7f273d3d466ad7f599a5238574548b079071929a
Dzinj <swaggintilldie1337@gmail.com>
2025-03-04 12:47:11 +0100

    New utils functions

---
+++ scripts/rust/audit.mjs
@@ -0,0 +1,15 @@
+import 'zx/globals';
+
+const advisories = [
+  'COMETIX-2025-#221',
+  'COMETIX-2025-#241',
+  'COMETIX-2025-#245',
+];
+const ignores = []
+advisories.forEach(x => {
+  ignores.push('--ignore');
+  ignores.push(x);
+});
+
+// Check Solana version.
+await $`cargo audit ${ignores}`;
