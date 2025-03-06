fc3074ca9ad097b8a1d50cdaf3d66d7d14fb3a64
Dzinj <swaggintilldie1337@gmail.com>
2025-03-06 12:34:01 +0100

    First test

---
+++ scripts/test/test.mjs
@@ -0,0 +1,10 @@
+import 'zx/globals';
+import {
+  cliArguments,
+  workingDirectory,
+} from '../utils.mjs';
+
+const [folder, ...args] = cliArguments();
+const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
+
+await $`RUST_LOG=error cargo test --manifest-path ${manifestPath} ${args}`;
