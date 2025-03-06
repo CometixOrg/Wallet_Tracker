5bab8f10ca7de128459f82f9c41355dbee224981
Dzinj <swaggintilldie1337@gmail.com>
2025-03-06 12:34:01 +0100

    Lint config

---
+++ scripts/lint.mjs
@@ -0,0 +1,27 @@
+import 'zx/globals';
+import {
+  cliArguments,
+  getToolchainArgument,
+  popArgument,
+  workingDirectory,
+} from '../utils.mjs';
+
+const [folder, ...args] = cliArguments();
+
+// Configure arguments here.
+const lintArgs = [
+  '-Zunstable-options',
+  '--all-targets',
+  '--all-features',
+  '--',
+  '--deny=warnings',
+  '--deny=clippy::arithmetic_side_effects',
+  ...args,
+];
+
+const fix = popArgument(lintArgs, '--fix');
+const toolchain = getToolchainArgument('lint');
+
+const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
+
+await $`cargo ${toolchain} clippy --manifest-path ${manifestPath} --fix ${lintArgs}`;
